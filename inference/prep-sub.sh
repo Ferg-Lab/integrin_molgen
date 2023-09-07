7b9c8a636daaf3e65d22ee3d991949314b0bce26
Siva Dasetty <siva.k.dasetty@gmail.com>
2023-09-07 01:07:59 -0500

    gmx files

---
+++ inference/prep-sub.sh
@@ -0,0 +1,46 @@
+# THIS SCRIPT IS TO GET THE INITIAL RMSD VALUES IN PLUMED FILE.
+
+for x in 'int1-int2' 'int1-open' 'int2-open'
+do
+
+cd $x
+for b in 'int1'
+do
+
+for i in {0..9}
+do
+
+echo $i
+rm -f -r target_md_"$b"_"$i"
+mkdir target_md_"$b"_"$i"
+cd target_md_"$b"_"$i"
+
+
+# int1 based
+cp ../../master_tmd/* .
+cp ../target_cg_resmapped_"$i"_"$b"_box.pdb .
+cp ../target_cg_resmapped_"$i"_"$b"_box_proa.pdb .
+cp ../target_cg_resmapped_"$i"_"$b"_box_prob.pdb .
+sed s/XXXX/$i/g  initrmsd_plumed.dat > temp.dat
+sed s/BBBB/$b/g temp.dat > temp2.dat
+mv temp2.dat  initrmsd_plumed.dat
+rm -f temp.dat
+
+# NOTE: equil-int1-7.gro can be obtained from https://github.com/tamarabidone/multiscale-model-alphaIIbBeta3.
+echo "gmx grompp -f run_init.mdp -c ../../all-atom/intermediate1_ff/equil/equil-int1-7.gro -p ../../all-atom/intermediate1_ff/topol.top -po run_init-out.mdp -o run_init.tpr" >> taac-init_run.sbatch
+echo -e "\n" >> taac-init_run.sbatch
+echo "gmx mdrun -s run_init.tpr -deffnm run_init-tmd -plumed initrmsd_plumed.dat" >> taac-init_run.sbatch
+
+sbatch taac-init_run.sbatch
+
+rm -f *#
+
+cd ..
+
+done
+
+done
+
+cd ..
+
+done
---
+++ inference/sub.sh
@@ -0,0 +1,48 @@
+# SCRIPT TO LAUNCH TARGETED MD SIMULATIONS
+for x in 'int1-int2' 'int1-open' 'int2-open'
+
+do
+
+cd $x
+for b in 'int1'
+do
+
+for i in {0..9} 
+do
+
+
+cd target_md_"$b"_"$i"
+
+
+# int1 based
+cp ../../master_tmd/targetmd_plumed.dat .
+cp ../../master_tmd/taac-run.sbatch .
+sed s/XXXX/$i/g targetmd_plumed.dat > temp.dat
+sed s/BBBB/$b/g temp.dat > temp2.dat
+mv temp2.dat targetmd_plumed.dat
+rm -f temp.dat
+init_rmsd_a=`(awk 'NR==2 {print $3}' init_rmsd_a_results.dat)`
+init_rmsd_b=`(awk 'NR==2 {print $3}' init_rmsd_b_results.dat)`
+sed s/FFFF/$init_rmsd_a/g targetmd_plumed.dat > temp.dat
+sed s/SSSS/$init_rmsd_b/g temp.dat > temp2.dat
+mv temp2.dat targetmd_plumed.dat
+rm -f temp.dat temp2.dat
+
+# NOTE: equil-int1-7.gro can be obtained from https://github.com/tamarabidone/multiscale-model-alphaIIbBeta3.
+echo "gmx grompp -f run.mdp -c ../../all-atom/intermediate1_ff/equil/equil-int1-7.gro -p ../../all-atom/intermediate1_ff/topol.top -po run-out.mdp -o run.tpr" >> taac-run.sbatch
+echo -e "\n" >> taac-run.sbatch
+echo "gmx mdrun -s run.tpr -deffnm run-tmd -plumed targetmd_plumed.dat" >> taac-run.sbatch 
+
+sbatch taac-run.sbatch
+
+rm -f *#
+
+cd ..
+
+done
+
+done
+
+cd ..
+
+done
